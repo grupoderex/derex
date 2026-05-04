2f93759103a260986361a2105a5910f3af13de1e
grupoderex <grupoderex@gmail.com>
2026-05-04 22:19:52 +0000

    cambio de numero de wpp flotante

--- derex-frontend-main/src/components/WhatsappFAB.tsx
+++ derex-frontend-main/src/components/WhatsappFAB.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { useEffect } from "react";
-import { usePathname } from "next/navigation";
 import { Icon } from "@iconify/react";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 
 export const WhatsappFAB = () => {
   const pathname = usePathname();
@@ -22,7 +22,7 @@ export const WhatsappFAB = () => {
   return (
     <div
       className="bg-green-500 w-14 h-14 flex items-center justify-center rounded-full shadow-lg fixed bottom-44 right-4 cursor-pointer z-19"
-      onClick={() => window.open("https://wa.me/5218111336614")}
+      onClick={() => window.open("https://wa.me/526621745601?text=Hola%20quisiera%20solicitar%20mas%20informaci%C3%B3n")}
     >
       <Icon icon="fa-brands:whatsapp" width="32" className="text-white" />
     </div>
