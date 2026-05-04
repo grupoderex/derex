56309d905cb7e6b77c677e348bea9b9465c167fc
grupojaver <grupojaver@gmail.com>
2026-05-04 22:51:44 +0000

    cambio de texto en boton wpp con acentos y mejorado

--- derex-frontend-main/src/components/WhatsappFAB.tsx
+++ derex-frontend-main/src/components/WhatsappFAB.tsx
@@ -22,7 +22,7 @@ export const WhatsappFAB = () => {
   return (
     <div
       className="bg-green-500 w-14 h-14 flex items-center justify-center rounded-full shadow-lg fixed bottom-44 right-4 cursor-pointer z-19"
-      onClick={() => window.open("https://wa.me/526621745601?text=Hola%20quisiera%20solicitar%20mas%20informaci%C3%B3n")}
+      onClick={() => window.open("https://wa.me/526621745601?text=Hola%2C%20me%20gustar%C3%ADa%20recibir%20m%C3%A1s%20informaci%C3%B3n%20sobre%20sus%20desarrollos.%20%C2%A1Gracias!")}
     >
       <Icon icon="fa-brands:whatsapp" width="32" className="text-white" />
     </div>
